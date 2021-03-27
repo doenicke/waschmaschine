faafd11331ad109af37fc9d7ebcec418c0e56404
u416641 <johannes.doenicke@lhsystems.com>
2021-03-27 21:59:34 +0100

    x

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 db_connection_str = 'mysql+pymysql://gast:gast@fhem.local/fhem'
-db_cache_file = 'df_sql.pkl'
+db_cache_file = '../data/df_sql.pkl'
 n_features = 60
 duration_min = 20   # Minuten
 model_file = 'model.pkl'
